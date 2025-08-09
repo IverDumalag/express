1e6a0a9f923ed04bad6fce1b38bb410320c8a370
alxyiko <alyumikogarcia@gmail.com>
2025-08-09 20:47:28 +0800

    8:47 PM

--- flutter_express/lib/0_components/help_widget.dart
+++ flutter_express/lib/0_components/help_widget.dart
@@ -18,12 +18,36 @@ class HelpIconWidget extends StatelessWidget {
         showDialog(
           context: context,
           builder: (context) => AlertDialog(
-            title: Text(helpTitle),
-            content: Text(helpText),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15.0),
+              side: BorderSide(
+                color: Color(0xFF334E7B),
+                width: 2,
+              ),
+            ),
+            backgroundColor: Colors.white,
+            elevation: 8,
+            title: Text(
+              helpTitle,
+              style: TextStyle(
+                color: Color(0xFF334E7B),
+                fontFamily: 'RobotoMono',
+                fontWeight: FontWeight.w500,
+                fontSize: 15,
+              ),
+            ),
+            content: Text(
+              helpText,
+              style: TextStyle(
+                color: Color(0xFF334E7B),
+                fontFamily: 'RobotoMono',
+                fontWeight: FontWeight.w500,
+              ),
+            ),
             actions: [
               TextButton(
                 onPressed: () => Navigator.of(context).pop(),
-                child: Text('Close'),
+                child: Text('OK', style: TextStyle(color: Color(0xFF2E5C9A), fontFamily: 'RobotoMono', fontWeight: FontWeight.w500)),
               ),
             ],
           ),
